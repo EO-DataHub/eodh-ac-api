1459768308350b62bddfd13984384e6f60adf4df
Michal Wierzbinski <michal.wierzbinski95@gmail.com>
2024-09-18 09:55:18 +0200

    feat(api): Add tests for GET job submissions endpoint

---
+++ tests/e2e/api/v1_0/test_action_creator.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+import pytest
+from starlette import status
+from starlette.testclient import TestClient
+
+from app import app
+
+client = TestClient(app)
+
+
+@pytest.mark.asyncio()
+async def test_get_job_submissions_endpoint_returns_forbidden_error_when_no_token_specified() -> None:
+    response = client.get("/api/v1.0/action-creator/submissions")
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.asyncio()
+async def test_get_job_submissions_endpoint_returns_unauthorized_error_when_bad_token() -> None:
+    response = client.get("/api/v1.0/action-creator/submissions", headers={"Authorization": "Bearer bad_token"})
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.asyncio()
+async def test_get_job_submissions_endpoint_returns_valid_response_when_all_is_ok(auth_token: str) -> None:
+    response = client.get("/api/v1.0/action-creator/submissions", headers={"Authorization": f"Bearer {auth_token}"})
+    assert response.status_code == status.HTTP_200_OK
+    assert all(k in response.json() for k in ("_links", "submitted_jobs"))
+    assert "self" in response.json()["_links"]
--- tests/e2e/api/v1_0/test_functions.py
+++ tests/e2e/api/v1_0/test_functions.py
@@ -12,14 +12,12 @@ client = TestClient(app)
 @pytest.mark.asyncio()
 async def test_functions_endpoint_returns_forbidden_error_when_no_token_specified() -> None:
     response = client.get("/api/v1.0/action-creator/functions")
-
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
 @pytest.mark.asyncio()
 async def test_functions_endpoint_returns_unauthorized_error_when_bad_token() -> None:
     response = client.get("/api/v1.0/action-creator/functions", headers={"Authorization": "Bearer bad_token"})
-
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
@@ -35,7 +33,8 @@ async def test_functions_endpoint_returns_all_functions(auth_token: str) -> None
 async def test_functions_endpoint_returns_functions_for_collection(auth_token: str) -> None:
     collection = "sentinel-2-l2a"
     response = client.get(
-        f"/api/v1.0/action-creator/functions?collection={collection}", headers={"Authorization": f"Bearer {auth_token}"}
+        f"/api/v1.0/action-creator/functions?collection={collection}",
+        headers={"Authorization": f"Bearer {auth_token}"},
     )
     assert response.status_code == status.HTTP_200_OK
     for f in response.json()["functions"]:
