5b6fb24f5074407b9de05f37098c7280128ea540
Michal Wierzbinski <michal.wierzbinski95@gmail.com>
2024-10-15 09:59:37 +0200

    refactor(tests): Move API tests to separate dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,17 +15,14 @@ if typing.TYPE_CHECKING:
     from _pytest.python import Function
     from fastapi import FastAPI
 
-MARKERS = ["unit", "integration", "e2e"]
-
 
 def pytest_collection_modifyitems(config: Config, items: list[Function]) -> None:  # noqa: ARG001
     rootdir = pathlib.Path(consts.directories.ROOT_DIR)
     for item in items:
         rel_path = pathlib.Path(item.fspath).relative_to(rootdir)
         mark_name = rel_path.as_posix().split("/")[1]
-        if mark_name in MARKERS:
-            mark = getattr(pytest.mark, mark_name)
-            item.add_marker(mark)
+        mark = getattr(pytest.mark, mark_name)
+        item.add_marker(mark)
 
 
 @pytest.fixture(name="app")
