bc2b87bb14f8ce1a2a7138ee54c25fc25dceff84
Michal Wierzbinski <michal.wierzbinski95@gmail.com>
2024-10-29 21:19:08 +0100

    feat(api): WIP. API v1.1 - enabling sequential WF execution for a demo

--- app.py
+++ app.py
@@ -6,6 +6,26 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.api.v1_0.action_creator.routes import action_creator_router_v1_0
 from src.api.v1_0.auth.routes import auth_router_v1_0
 from src.api.v1_0.health.routes import health_router_v1_0
+from src.api.v1_1.action_creator.routes import action_creator_router_v1_1
+
+
+def register_api_v1_0(app: FastAPI) -> FastAPI:
+    v1_0_app = FastAPI(title="EOPro Action Creator API", version="1.0.0", description="API for Action Creator.")
+    v1_0_app.include_router(health_router_v1_0)
+    v1_0_app.include_router(auth_router_v1_0)
+    v1_0_app.include_router(action_creator_router_v1_0)
+    app.mount("/api/v1.0", v1_0_app)
+    return v1_0_app
+
+
+def register_api_v1_1(app: FastAPI) -> FastAPI:
+    v1_1_app = FastAPI(title="EOPro Action Creator API", version="1.1.0", description="API for Action Creator.")
+    v1_1_app.include_router(health_router_v1_0)
+    v1_1_app.include_router(auth_router_v1_0)
+    v1_1_app.include_router(action_creator_router_v1_1)
+    app.mount("/api/v1.1", v1_1_app)
+    return v1_1_app
+
 
 app = FastAPI(
     title="EOPro Action Creator API",
@@ -14,16 +34,9 @@ app = FastAPI(
     docs_url=None,
 )
 
-v1_0_app = FastAPI(
-    title="EOPro Action Creator API", version="1.0.0", description="Mockup of an API for Action Creator."
-)
-
-v1_0_app.include_router(health_router_v1_0)
-v1_0_app.include_router(auth_router_v1_0)
-v1_0_app.include_router(action_creator_router_v1_0)
-
-app.mount("/api/v1.0", v1_0_app)
-app.mount("/api/latest", v1_0_app)
+app_v1_0 = register_api_v1_0(app)
+app_v1_1 = register_api_v1_1(app)
+app.mount("/api/latest", app_v1_1)
 
 app.add_middleware(
     CORSMiddleware,
--- tests/unit/services/test_validation_utils.py
+++ tests/unit/services/test_validation_utils.py
@@ -2,14 +2,14 @@ from __future__ import annotations
 
 import pytest
 
+from src.consts.aoi import HEATHROW_AOI, UK_AOI
 from src.services.validation_utils import ensure_area_smaller_than
-from tests.api.v1_0.action_creator.schemas.test_functions import TEST_HEATHROW_AOI, TEST_UK_AOI
 
 
 def test_ensure_area_smaller_than_func_rises_error() -> None:
     with pytest.raises(ValueError, match="Area exceeds 386.10 square miles."):
-        ensure_area_smaller_than(geom=TEST_UK_AOI, area_size_limit=1000)
+        ensure_area_smaller_than(geom=UK_AOI, area_size_limit=1000)
 
 
 def test_ensure_area_smaller_than_func_does_not_rais_an_error() -> None:
-    ensure_area_smaller_than(geom=TEST_HEATHROW_AOI, area_size_limit=1000)
+    ensure_area_smaller_than(geom=HEATHROW_AOI, area_size_limit=1000)
