d5bbbf608a99902ddbab6930c030f25bf1b03a5e
Michal Wierzbinski <michal.wierzbinski95@gmail.com>
2024-12-09 09:10:35 +0100

    feat(api): EODH WS HTTP client

---
+++ http-clients/eodh-workspaces/set_creds.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import json
+from pathlib import Path
+
+template = """
+[default]
+aws_access_key_id={accessKeyId}
+aws_secret_access_key={secretAccessKey}
+aws_session_token={sessionToken}
+"""
+
+OUT_PATH = Path("~/.aws/credentials")
+
+
+def main() -> None:
+    cred_file = Path("tmp_s3_credentials.json")
+    data = json.loads(cred_file.read_text())
+    output_content = template.format(**data)
+    OUT_PATH.write_text(output_content, encoding="utf-8")
+
+
+if __name__ == "__main__":
+    main()
