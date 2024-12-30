b1c07522c967e02ef11af43dfbf51921d8c66053
Michal Wierzbinski <michal.wierzbinski95@gmail.com>
2024-12-30 12:45:34 +0100

    feat(api): Add date range validation for STAC collections

--- src/services/validation_utils.py
+++ src/services/validation_utils.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import json
+from datetime import datetime, timezone
 from typing import TYPE_CHECKING, Any
 
 import pyproj
@@ -13,13 +14,42 @@ from src.api.v1_1_1.action_creator.functions import FUNCTIONS_REGISTRY as NEW_FU
 from src.consts.action_creator import FUNCTIONS_REGISTRY
 
 if TYPE_CHECKING:
-    from datetime import datetime
-
     import shapely.geometry
 
 EXPECTED_BBOX_ELEMENT_COUNT = 4
 MAX_AREA_SQ_KM = 1000
 SQ_MILES_DIVISOR = 2.59
+
+STAC_COLLECTION_DATE_RANGE_LOOKUP = {
+    "sentinel-1-grd": (
+        datetime.fromisoformat("2014-10-10T10:28:21+00:00"),
+        None,
+    ),
+    "sentinel-2-l1c": (
+        datetime.fromisoformat("2015-06-27T10:25:31+00:00"),
+        None,
+    ),
+    "sentinel-2-l2a": (
+        datetime.fromisoformat("2015-06-27T10:25:31+00:00"),
+        None,
+    ),
+    "sentinel-2-l2a-ard": (
+        datetime.fromisoformat("2015-06-27T10:25:31+00:00"),
+        None,
+    ),
+    "esacci-globallc": (
+        datetime.fromisoformat("1992-01-01T00:00:00+00:00"),
+        datetime.fromisoformat("2015-12-31T23:59:59+00:00"),
+    ),
+    "clms-corinelc": (
+        datetime.fromisoformat("1990-01-01T00:00:00+00:00"),
+        datetime.fromisoformat("2018-12-31T23:59:59+00:00"),
+    ),
+    "clms-water-bodies": (
+        datetime.fromisoformat("2020-01-01T00:00:00+00:00"),
+        None,
+    ),
+}
 
 
 class AreaOfInterestTooBigError:
@@ -70,6 +100,29 @@ class InvalidDateRangeError:
             "invalid_date_range_error",
             "End date cannot be before start date.",
             {"date_start": date_start, "date_end": date_end},
+        )
+
+
+class StacDateRangeError:
+    @classmethod
+    def make(
+        cls,
+        collection: str,
+        valid_start: datetime | None = None,
+        valid_end: datetime | None = None,
+        date_start: datetime | None = None,
+        date_end: datetime | None = None,
+    ) -> PydanticCustomError:
+        return PydanticCustomError(
+            "stac_date_range_error",
+            "Invalid date range for selected STAC collection: {collection}. Valid range is between {valid_start} and {valid_end}.",  # noqa: E501
+            {
+                "valid_start": valid_start.isoformat() if valid_start else None,
+                "valid_end": valid_end.isoformat() if valid_end else None,
+                "date_start": date_start,
+                "date_end": date_end,
+                "collection": collection,
+            },
         )
 
 
@@ -138,6 +191,48 @@ def validate_stac_collection_v1_1_1(specified_collection: str, function_identifi
             collection=specified_collection,
             valid_collections=valid_collections,
             function_identifier=function_identifier,
+        )
+
+
+def validate_stac_date_range(
+    stac_collection: str,
+    date_start: datetime | None = None,
+    date_end: datetime | None = None,
+) -> None:
+    valid_date_min, valid_date_max = STAC_COLLECTION_DATE_RANGE_LOOKUP[stac_collection]
+    if date_start is not None:
+        date_start = (
+            date_start.replace(tzinfo=timezone.utc)
+            if date_start.tzinfo is None
+            else date_start.astimezone(tz=timezone.utc)
+        )
+    if date_end is not None:
+        date_end = (
+            date_end.replace(tzinfo=timezone.utc) if date_end.tzinfo is None else date_end.astimezone(tz=timezone.utc)
+        )
+
+    if date_start is not None and (
+        (valid_date_min is not None and date_start < valid_date_min)
+        or (valid_date_max is not None and date_start > valid_date_max)
+    ):
+        raise StacDateRangeError.make(
+            collection=stac_collection,
+            date_start=date_start,
+            date_end=date_end,
+            valid_start=valid_date_min,
+            valid_end=valid_date_max,
+        )
+
+    if date_end is not None and (
+        (valid_date_min is not None and date_end < valid_date_min)
+        or (valid_date_max is not None and date_end > valid_date_max)
+    ):
+        raise StacDateRangeError.make(
+            collection=stac_collection,
+            date_start=date_start,
+            date_end=date_end,
+            valid_start=valid_date_min,
+            valid_end=valid_date_max,
         )
 
 
