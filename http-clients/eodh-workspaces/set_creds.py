f563db2a39f39646064cd4b54eba83e2da24db13
Michal Wierzbinski <michal.wierzbinski95@gmail.com>
2025-07-24 12:04:04 +0200

    feat(ci): Remove old rules, fix path encoding issue for http-clients

--- http-clients/eodh-workspaces/set_creds.py
+++ http-clients/eodh-workspaces/set_creds.py
@@ -15,7 +15,7 @@ OUT_PATH = Path("~/.aws/credentials")
 
 def main() -> None:
     cred_file = Path("tmp_s3_credentials.json")
-    data = json.loads(cred_file.read_text())
+    data = json.loads(cred_file.read_text(encoding="utf-8"))
     output_content = template.format(**data)
     OUT_PATH.write_text(output_content, encoding="utf-8")
 
